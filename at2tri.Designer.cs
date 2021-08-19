26293c770e8caf7420b37ee68c68f45a4486f19c
Alvaro-Siqueira-Silva <84384180+Alvaro-Siqueira-Silva@users.noreply.github.com>
2021-08-19 18:52:47 -0300

    Add files via upload

---
+++ at2tri.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace earsras
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpeso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtalt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtpeso
+            // 
+            this.txtpeso.Location = new System.Drawing.Point(37, 64);
+            this.txtpeso.Name = "txtpeso";
+            this.txtpeso.Size = new System.Drawing.Size(170, 20);
+            this.txtpeso.TabIndex = 0;
+            this.txtpeso.Text = "peso total";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pontes sp, veja quais tu podes passar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(93, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // txtalt
+            // 
+            this.txtalt.Location = new System.Drawing.Point(70, 90);
+            this.txtalt.Name = "txtalt";
+            this.txtalt.Size = new System.Drawing.Size(100, 20);
+            this.txtalt.TabIndex = 5;
+            this.txtalt.Text = "altura";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(263, 300);
+            this.Controls.Add(this.txtalt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpeso);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpeso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtalt;
+    }
+}
+
---
+++ at2tri.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace earsras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1, n2;
+
+            n1 = Convert.ToDouble(txtpeso.Text);
+            n2 = Convert.ToDouble(txtalt.Text);
+            if (n1 >= 20000 || n2 >= 20)
+            {
+                MessageBox.Show("Você não pode passar pelas pontes: Cardoso, Ferreira, Naguaré "
+                    + "Você não pode passar por baixo das pontes: Cardoso, Ferreira, Naguaré");
+            }
+
+
+            else if (n1 <= 19999 || n2 <= 19 && n2>= 15 || n1 >= 10000)
+            {
+                MessageBox.Show("Você não pode passar pelas pontes: Cardoso, Ferreira "
+                    + "Você não pode passar por baixo das pontes: Cardoso, Ferreira");
+            }
+
+            else if (n1 <= 9999 || n2 <= 14 && n2 >= 9 || n1 >= 5000)
+            {
+                MessageBox.Show("Você não pode passar pelas pontes: Cardoso "
+                    + "Você não pode passar por baixo das pontes: Cardoso");
+            }
+
+            else if (n1 <= 4999 || n2 <=9 )
+            {
+                MessageBox.Show("Você pode passar por baixo e por cima de todas as pontes");
+            }
+
+        }
+    }
+}
